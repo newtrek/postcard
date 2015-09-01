d1e59b65a09d55600aff9037a98de3eb53c6f52c
Protrek <protrek@HyeongrokLeeui-MacBook-Pro.local>
2015-09-01 10:49:32 +0900

    Initial Commit

---
+++ postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  postcard
+//
+//  Created by Protrek on 2015. 9. 1..
+//  Copyright (c) 2015년 Protrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
